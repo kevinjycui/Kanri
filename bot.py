a7861cf0faa7a0b1366dc00ecef4b5b05623ab2b
Nicholas Stewart <nicktstewart@gmail.com>
2021-01-30 23:14:20 +0900

    changed requirements

--- bot.py
+++ bot.py
@@ -2,9 +2,14 @@ from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 from processor import respond
+from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 
+slack_events_adapter = SlackEventAdapter("20f2f470e8cee9a57c03515479effe40", "/slack/events", app)
+@slack_events_adapter.on("challenge")
+def catchChallenge(challenge_data):
+	return(challenge_data.challenge)
 
 @app.route('/bot', methods=['POST'])
 def bot():
