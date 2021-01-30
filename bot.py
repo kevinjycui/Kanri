53f601a640c863229dcbf1a3836ae09cccdc7818
Nicholas Stewart <nicktstewart@gmail.com>
2021-01-31 00:27:58 +0900

    a bunch of edits

--- bot.py
+++ bot.py
@@ -1,15 +1,34 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 from processor import respond
 from slackeventsapi import SlackEventAdapter
+import os
+from threading import Thread
 
 app = Flask(__name__)
 
 slack_events_adapter = SlackEventAdapter("20f2f470e8cee9a57c03515479effe40", "/slack/events", app)
-@slack_events_adapter.on("challenge")
-def catchChallenge(challenge_data):
-	return(challenge_data.challenge)
+
+@slack_events_adapter.on("url_request")
+def handle_message(request):
+        app.logger.info(request.method)
+        json_dict = json.loads(request.body.decode("utf-8"))
+        
+
+@app.route('/')
+def event_hook(request):
+    app.logger.info(request.method)
+    json_dict = json.loads(request.body.decode("utf-8"))
+    if json_dict["token"] != VERIFICATION_TOKEN:
+        return {"status": 403}
+
+    if "type" in json_dict:
+        if json_dict["type"] == "url_verification":
+            response_dict = {"challenge": json_dict["challenge"]}
+            return response_dict
+    return {"status": 500}
+    return
 
 @app.route('/bot', methods=['POST'])
 def bot():
